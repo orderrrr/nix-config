b19b5d0ae950907601e2a40aa2b55a961153172e
nmcintosh <nmcintosh@eus.v.co.zw>
2024-12-24 11:46:45 +0200

    luasnip

--- dotfiles/config/nvim/lua/plugins/completion.lua
+++ dotfiles/config/nvim/lua/plugins/completion.lua
@@ -4,19 +4,6 @@ return {
         {
             'L3MON4D3/LuaSnip',
             version = "v2.*",
-            config = function()
-                local ls = require("luasnip")
-
-                ls.setup()
-
-                ls.config.set_config {}
-                ls.auto_snippets = true
-
-                require("plugins.luasnip.ft.all")
-                require("plugins.luasnip.ft.java")
-                require("plugins.luasnip.ft.sql")
-                require("plugins.luasnip.ft.typescript")
-            end,
         }
     },
     -- use a release tag to download pre-built binaries
