b2d8130a0bfea349a31b7ad1469ba22eb3f19871
nmcintosh <nmcintosh@eus.v.co.zw>
2025-01-02 10:42:27 +0200

    obsidian follow url

--- dotfiles/config/nvim/lua/plugins/init.lua
+++ dotfiles/config/nvim/lua/plugins/init.lua
@@ -128,7 +128,14 @@ return {
                     path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/vault",
                 },
             },
-            ui = { enable = false }
+            ui = { enable = false },
+            follow_url_func = function(url)
+                -- Open the URL in the default web browser.
+                vim.fn.jobstart({ "open", url }) -- Mac OS
+                -- vim.fn.jobstart({"xdg-open", url})  -- linux
+                -- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
+                -- vim.ui.open(url) -- need Neovim 0.10.0+
+            end,
         },
     },
     {
