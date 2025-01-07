fc782d80baf81a2ff6f9d08f2097defbaf3fd1f9
nmcintosh <nmcintosh@eus.v.co.zw>
2025-01-07 11:51:17 +0200

    spectre

--- dotfiles/config/nvim/lua/plugins/init.lua
+++ dotfiles/config/nvim/lua/plugins/init.lua
@@ -1,7 +1,24 @@
 return {
     -- Useful plugin to show you pending keybinds.
-    { 'folke/which-key.nvim',   opts = {} },
-    { 'nvim-pack/nvim-spectre', opts = {} },
+    { 'folke/which-key.nvim',  opts = {} },
+    {
+        'nvim-pack/nvim-spectre',
+        opts = {},
+        config = function()
+            vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
+                desc = "Toggle Spectre"
+            })
+            vim.keymap.set('n', '<leader>Sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+                desc = "Search current word"
+            })
+            vim.keymap.set('v', '<leader>Sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+                desc = "Search current word"
+            })
+            vim.keymap.set('n', '<leader>Sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+                desc = "Search on current file"
+            })
+        end
+    },
     { 'unblevable/quick-scope' },
     {
         'stevearc/dressing.nvim',
