258a052637311f38de1e5f2b84c74dcaf89c7fb5
nmcintosh <nmcintosh@eus.v.co.zw>
2025-03-17 10:19:40 +0200

    flutter

--- dotfiles/config/nvim/lua/plugins/lsp.lua
+++ dotfiles/config/nvim/lua/plugins/lsp.lua
@@ -351,26 +351,26 @@ return {
         }
       })
 
-      -- require("flutter-tools").setup({
-      --   lsp = {
-      --     color = { -- show the derived colours for dart variables
-      --       enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
-      --       background = false, -- highlight the background
-      --       background_color = nil, -- required, when background is transparent (i.e. background_color = { r = 19, g = 17, b = 24},)
-      --       foreground = false, -- highlight the foreground
-      --       virtual_text = true, -- show the highlight using virtual text
-      --       virtual_text_str = "■", -- the virtual text character to highlight
-      --     },
-      --     on_attach = on_attach,
-      --     capabilities = function(config)
-      --       return vim.tbl_deep_extend(
-      --         'force',
-      --         config,
-      --         capabilities
-      --       )
-      --     end,
-      --   }
-      -- })
+      require("flutter-tools").setup({
+        lsp = {
+          color = { -- show the derived colours for dart variables
+            enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+            background = false, -- highlight the background
+            background_color = nil, -- required, when background is transparent (i.e. background_color = { r = 19, g = 17, b = 24},)
+            foreground = false, -- highlight the foreground
+            virtual_text = true, -- show the highlight using virtual text
+            virtual_text_str = "■", -- the virtual text character to highlight
+          },
+          on_attach = on_attach,
+          capabilities = function(config)
+            return vim.tbl_deep_extend(
+              'force',
+              config,
+              capabilities
+            )
+          end,
+        }
+      })
 
       require("lspconfig").clangd.setup {
         on_attach = on_attach,
