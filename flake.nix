a4ee3327b145f947ae8e552c4b1e3b6a9ce4f20b
nmcintosh <nmcintosh@eus.v.co.zw>
2025-04-28 19:35:04 +0200

    sync

--- flake.nix
+++ flake.nix
@@ -9,22 +9,44 @@
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, nix-darwin, ... }:
+  outputs = inputs@{ self, nixpkgs, home-manager, nix-darwin, nix-homebrew, ... }:
   {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#nathaniels-MacBook-Pro
     darwinConfigurations."nathaniels-MacBook-Pro" = nix-darwin.lib.darwinSystem {
       modules = [ 
-                    ./configuration.nix
-                    home-manager.darwinModules.home-manager
-                    {
-                        home-manager.useGlobalPkgs = true;
-                        home-manager.useUserPackages = true;
-                        home-manager.users.nmcintosh = import ./home.nix;
-                    }
-                ];
+        ./configuration.nix
+
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            enable = true;
+            enableRosetta = true;
+            user = "nmcintosh";
+            autoMigrate = true;
+          };
+        }
+
+        home-manager.darwinModules.home-manager
+        {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.nmcintosh = import ./home.nix;
+        }
+      ];
     };
 
     # Expose the package set, including overlays, for convenience.
--- home.nix
+++ home.nix
@@ -13,7 +13,6 @@
   # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
   home.packages = with pkgs; [
-    pkgs.orbstack
   ];
 
   home.file = {
