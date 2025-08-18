972dc9ce281cd1897e14a94c3ba4422c9af8bf85
nmcintosh <nmcintosh@eus.v.co.zw>
2025-08-18 11:10:32 +0200

    keymaps

--- dotfiles/config/nvim/lua/plugins/ai.lua
+++ dotfiles/config/nvim/lua/plugins/ai.lua
@@ -3,3 +3,12 @@ vim.pack.add({
 })
 
 require("opencode").setup({});
+
+vim.keymap.set('n', '<leader>ot', function() require('opencode').toggle() end)
+vim.keymap.set('n', '<leader>oa', function() require('opencode').ask('@cursor: ') end)
+vim.keymap.set('v', '<leader>oa', function() require('opencode').ask('@selection: ') end)
+vim.keymap.set({ 'n', 'v' }, '<leader>op', function() require('opencode').select_prompt() end)
+vim.keymap.set('n', '<leader>on', function() require('opencode').command('session_new') end)
+vim.keymap.set('n', '<leader>oy', function() require('opencode').command('messages_copy') end)
+vim.keymap.set('n', '<S-C-u>', function() require('opencode').command('messages_half_page_up') end)
+vim.keymap.set('n', '<S-C-d>', function() require('opencode').command('messages_half_page_down') end)
