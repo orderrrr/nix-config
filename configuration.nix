b621e9eec1b3f6d316bd866c6e5ed65591a7051a
Nathaniel McIntosh <nathanielstewartmcintosh@gmail.com>
2025-11-17 08:48:45 +0200

    work

--- configuration.nix
+++ configuration.nix
@@ -29,9 +29,10 @@
     brews = [
       "ansible" "ansible-lint" "btop" "cmake" "coreutils" "curl" "docker" "docker-compose" "direnv"
       "eza" "ffmpeg" "fzf" "fish" "gh" "git" "graphviz" "imagemagick" "ipatool" "lazygit" "maven"
-       "neofetch" "neovim" "nvm" "ollama" "python@3.10" "rustup" "spirv-cross"
+       "neofetch" "neovim" "ollama" "python@3.10" "rustup" "spirv-cross" "fisher"
       "sshpass" "sst/tap/opencode" "starship" "tailscale" "telnet" "tinyxml2" "typescript"
       "vulkan-loader" "wget" "yt-dlp" "zellij" "zig" "zoxide" "jj" "pixi" "colmap" "just"
+			"llvm@20"
 
 			# "krunvm" "podman"
 			"molten-vk"
